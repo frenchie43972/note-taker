4b5c2e2b14739e2d474b62e0958341e93d67f515
Kris French <frenchie43972@gmail.com>
2026-01-05 08:14:04 +0900

    Add more files and folders

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const notesRouter = require('./routes/notes.js');
+const db = require('./db.js');
 
 const app = express();
 
+app.use(express.json());
 app.use('/notes', notesRouter);
 
 module.exports = app;
