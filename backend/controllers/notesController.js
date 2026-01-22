18010595b0742e01fe9d4686a7dfb377cc6d3c49
Kris French <frenchie43972@gmail.com>
2026-01-22 13:54:08 +0900

    Backend finished

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -2,24 +2,143 @@
   Controller (action handlers) for note-related actions.
   Each function will corresond to one type of CRUD operation
 */
+import { error } from "console";
+import db from "../db/database.js";
+
+/*
+  GET /notes
+  Returns all notes from the DB
+*/
 export function getAllNotes(req, res) {
-  /*
-    this will eventually handle the SQL logic to get * from notes
-    for now justa simple response to get it working
-  */
-  res.json({
-    message: 'getAllNotes working',
+  const sql = "SELECT id, title, body, created_at FROM notes";
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to fetch notes" });
+
+      return;
+    }
+    res.json(rows);
   });
 }
 
+/*
+  GET /notes/:id
+  Returns one note by id
+*/
+export function getNoteById(req, res) {
+  const { id } = req.params;
+
+  const sql = `SELECT id, title, body, created_at FROM notes WHERE id = ?`;
+
+  db.get(sql, [id], (err, row) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to fetch note" });
+
+      return;
+    }
+
+    if (!row) {
+      res.status(404).json({ error: "Note not found" });
+
+      return;
+    }
+
+    res.json(row);
+  });
+}
+
+/*
+  POST /notes
+  Creats a note after input is validated
+*/
 export function createNote(req, res) {
-  /*
-    This will eventually hold the POST
-    - read the title and body from the req.body
-    - validate input
-    - insert a new note into the DB
-  */
-  res.status(501).json({
-    message: 'createNote not implemented',
+  const { title, body } = req.body;
+
+  if (!title || !body) {
+    res.status(400).json({ error: "title and body are required" });
+  }
+
+  const sql = ` INSERT INTO notes (title, body) VALUES (?, ?)`;
+
+  db.run(sql, [title, body], function (err) {
+    if (err) {
+      res.status(500).json({ error: "Failed to create note" });
+
+      return;
+    }
+
+    res.status(201).json({
+      id: this.lastID,
+      title,
+      body,
+    });
+  });
+}
+
+/*
+  DELETE /notes/:id
+  Deletes a not by its id
+*/
+export function deleteNote(req, res) {
+  // id comes from the URL, not the body so use req.params instead of req.body
+  const { id } = req.params;
+
+  const sql = "DELETE FROM notes WHERE id = ?";
+
+  db.run(sql, [id], function (err) {
+    if (err) {
+      res.status(500).json({ error: "Failed to delete note" });
+
+      return;
+    }
+
+    // this.changes tells how many rows were affected
+    if (this.changes === 0) {
+      res.status(404).json({ error: "Note not found" });
+
+      return;
+    }
+
+    res.status(204).send();
+  });
+}
+
+/*
+  PUT /notes/:id
+  Update an existing note's title and body.
+*/
+export function updateNotes(req, res) {
+  const { id } = req.params;
+  const { title, body } = req.body;
+
+  if (!title || !body) {
+    res.status(400).json({ error: "title and body are required" });
+
+    return;
+  }
+
+  const sql = `
+      UPDATE notes
+      SET title = ?, body = ?
+      WHERE id = ?
+    `;
+
+  db.run(sql, [title, body, id], function (err) {
+    if (err) {
+      res.status(500).json({ error: "Failed to update note" });
+
+      return;
+    }
+
+    if (this.changes === 0) {
+      res.status(404).json({ error: "Note not found" });
+    }
+
+    res.status(200).json({
+      id,
+      title,
+      body,
+    });
   });
 }
--- backend/routes/notesRouter.js
+++ backend/routes/notesRouter.js
@@ -1,5 +1,11 @@
-import express from 'express';
-import { getAllNotes, createNote } from '../controllers/notesController.js';
+import express from "express";
+import {
+  getAllNotes,
+  createNote,
+  deleteNote,
+  updateNotes,
+  getNoteById,
+} from "../controllers/notesController.js";
 
 const router = express.Router();
 
@@ -7,12 +13,30 @@ const router = express.Router();
   GET /notes
   This route will return a list of notes.
 */
-router.get('/', getAllNotes);
+router.get("/", getAllNotes);
+
+/*
+  GET /notes/:id
+  This route will return one note.
+*/
+router.get("/:id", getNoteById);
 
 /**
     POST /notes
     This route creates a new note.
  */
-router.post('/', createNote);
+router.post("/", createNote);
+
+/*
+  DELETE /notes
+  This route deletes a not by id
+*/
+router.delete("/:id", deleteNote);
+
+/* 
+  PUT /notes
+  This route updates notes by id 
+*/
+router.put("/:id", updateNotes);
 
 export default router;
